f2954917bdfad7dd373a13975ef09285c0f7c1f8
saumya <saumyaphonu@gmail.com>
2020-03-31 17:33:07 +0530

    First working crawler->subject to more testing

--- Crawler.py
+++ Crawler.py
@@ -3,13 +3,13 @@ from Extra import *
 class Crawler(Extra):
 
     def crawl(self):
-        i=0
-        try:
-            while i<len(self.urls):
 
+        i=0
+
+        while i<len(self.urls):
+            try:
                 self.cur_page = self.urls[i]
                 soup=BeautifulSoup(ur.urlopen(self.urls[i]), 'lxml')
-
 
                 for t in soup.find_all('a'):
                     path = t['href']
@@ -17,15 +17,22 @@ class Crawler(Extra):
                     u = url_normalize(self.cur_page, path)
 
                     if u is not None:
-                        if target_test(u):
-                            if u not in self.tsites:
-                                self.tsites.append(u)
-
-                        else:
-                            if u not in self.urls:
-                                self.urls.append(u)
-
+                        if u not in self.urls:
+                            self.urls.append(u)
+                            print(u)
                 i+=1
 
-        except Exception as e:
-            print(e, i, self.urls[i])
+
+            except Exception as e:
+                print(e, self.urls[i])
+                print(len(self.urls))
+                i+=1
+
+
+a='https://www.bitmesra.ac.in/BIT_Mesra?cid=8&pid=H'
+
+web=Crawler(ur.urlopen(a), 'lxml')
+web.home_page=a
+web.urls.append(a)
+
+web.crawl()
--- utilities.py
+++ utilities.py
@@ -2,9 +2,15 @@ from modules import *
 
 def url_normalize(cur_page, path):
 
+    cur_page=cur_page.strip()
+    path=path.strip()
+
     a=list(up.urlparse(cur_page))
 
     b=list(up.urlparse(path))
+
+    if b[2]=='':
+        return None
 
     if b[1]!='' and b[1]!=a[1]:   #sub-domain or external site
         s1=extract_domain(a[1])
@@ -19,8 +25,29 @@ def url_normalize(cur_page, path):
         else:        #external site
             return None
 
+
     if b[1]=='' or b[1]==a[1]:  #link belonging to the same domain
-        pass
+        a=up.urlunparse(a)
+        if a[-1]=='/':     #Processing 'a'
+            a=a[:-1]
+
+        if b[2][:3]=='../':
+
+            b[2]=b[2][2:]
+
+            i=-1
+            while a[i]!='/':
+                i-=1
+            a=a[:i]
+
+        else:
+            if b[2][:2]=='./':
+                b[2]=b[2][1:]
+
+            if b[2][0]!='/':
+                b[2] = '/'+b[2]
+
+        return a + b[2]
 
 
 
