bb4c6d1cc0efbed8ee408464b586d12422d1f8c7
saumya <saumyaphonu@gmail.com>
2020-06-23 20:23:58 +0530

    work on scraping data from google started -> improvement

--- scrape_from_google.py
+++ scrape_from_google.py
@@ -1,27 +1,31 @@
 from url_utilities import *
 from db_utilities import *
 
-try:
-    ht = load_page('https://www.google.com/search?client=ubuntu&hs=MLS&sa=X&channel=fs&sz=0&biw=1920&bih=857&q=schools+in+patna&npsic=0&rflfq=1&rlha=0&rllag=25616605,85092215,1401&tbm=lcl&ved=2ahUKEwiLhqeujpjqAhVBX30KHfi6D5oQjGp6BAgMED4&rldoc=1#rlfi=hd:;si:;mv:[[25.669160163970666,85.3535601189149],[25.448635381955807,84.83857598805552]];start:260')
-    soup = BeautifulSoup(ht.read(), features='lxml', parse_only=SoupStrainer('div', attrs={'class':'uMdZh rl-qs-crs-t mnr-c'}))
-    ht.close()
-except Exception as e:
-    print(e)
+start = 0
+b = set()
+for start in range(0, 320):
 
-else:
-    i = 0
-    a = soup.find_all('div', attrs={'class':'uMdZh rl-qs-crs-t mnr-c'})
-    n = len(a)
+    try:
+        ht = load_page('https://www.google.com/search?client=ubuntu&hs=MLS&sa=X&channel=fs&sz=0&biw=1920&bih=857&q=schools+in+patna&npsic=0&rflfq=1&rlha=0&rllag=25616605,85092215,1401&tbm=lcl&ved=2ahUKEwiLhqeujpjqAhVBX30KHfi6D5oQjGp6BAgMED4&rldoc=1#rlfi=hd:;si:;mv:[[25.669160163970666,85.3535601189149],[25.448635381955807,84.83857598805552]];start:'+str(start))
+        soup = BeautifulSoup(ht.read(), features='lxml', parse_only=SoupStrainer('div', attrs={'class':'uMdZh rl-qs-crs-t mnr-c'}))
+        ht.close()
+    except Exception as e:
+        print(e)
 
-    if n > 20:
-        i = 3
+    else:
+        i = 0
+        a = soup.find_all('div', attrs={'class':'uMdZh rl-qs-crs-t mnr-c'})
+        n = len(a)
 
-    for i in range(0, n):
-        name = a[i].find('div').find('div').find('div').find('div')
-        print(name.prettify())
-        print()
+        i = n-1
 
-    print(n)
-    # eg = soup.find('div')
+        while i>=0 and i >= n-20:    # Scraping only the last 20 blocks
+            name = a[i].find('div').find('div').find('div').find('div')
+            b.add(name.get_text())
+            i -= 1
 
-    # print(eg.prettify())
+    start += 20
+
+for i in b:
+    print(i)
+
