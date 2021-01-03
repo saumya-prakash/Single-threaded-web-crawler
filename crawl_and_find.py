1c61bac642f1f2be0b7d306408bad1f33fac9414
saumya-prakash <saumyaphonu@gmail.com>
2021-01-03 17:16:19 +0530

    cleaning

--- crawl_and_find.py
+++ crawl_and_find.py
@@ -16,7 +16,7 @@ else:
     curs = mycon.cursor()
 
     # basic selection query
-    query = " SELECT id, home_page, examined_pages, complete_crawl FROM records where link_found = 'n' and id between 900 and 1000 "
+    query = " SELECT id, home_page, examined_pages, complete_crawl FROM records where link_found = 'n' and id between 600 and 700 "
     curs.execute(query)
 
     a = curs.fetchall()
